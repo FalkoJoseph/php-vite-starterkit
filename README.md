82a999077d6f9bb814fdcfb446dfd8a7f52efad5
Falko Joseph <falkojoseph@gmail.com>
2023-05-06 17:54:36 +0200

    🪴 feat: initial setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vite Setup
+
+## Features
+
+- Live reload
+- Tailwind 3
+- Vue 3
+- Prettier
+
+## Usage
+
+- `npm run dev` - Start dev server
+- `npm run build` - Build for production
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="text-3xl">Vue app cats knoekels</h1>
+    <div class="count">
+      <p>{{ count }}</p>
+    </div>
+    <button @click="increment">Counter</button>
+    {{ logo }}
+    <img :src="`dist${logo}`" class="logo vue" alt="Vue logo" />
+  </div>
+</template>
+
+<script>
+import logo from "./assets/svg/vite.svg";
+
+export default {
+  data() {
+    return {
+      count: 0,
+      logo,
+    };
+  },
+  computed: {},
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
